6dda7aaabc46b372058d4f2982baa398bb71419d
Takuya Kikuchi <takuyakikuch@gmail.com>
2022-05-08 16:27:54 +0900

    exercise 4: speciality story grid

--- src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.js
+++ src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components/macro';
+import { QUERIES } from '../../constants';
 
 import { MARKET_DATA, SPORTS_STORIES } from '../../data';
 
@@ -47,14 +48,55 @@ const SpecialtyStoryGrid = () => {
 const Wrapper = styled.div`
   display: grid;
   gap: 48px;
+
+  @media ${QUERIES.laptopAndUp} {
+    grid-template-columns: 1fr 1fr;
+    gap: 0;
+  }
 `;
 
-const MarketsSection = styled.section``;
+const MarketsSection = styled.section`
+  @media ${QUERIES.laptopAndUp} {
+    padding-right: 16px;
+    border-right: 1px solid var(--color-gray-300); 
+  }
+`;
 
-const MarketCards = styled.div``;
+const MarketCards = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 16px;
 
-const SportsSection = styled.section``;
+  @media ${QUERIES.tabletAndUp} {  
+    grid-template-columns: repeat(4, 1fr);
+  }
 
-const SportsStories = styled.div``;
+  @media ${QUERIES.laptopAndUp} {  
+    grid-template-columns: repeat(3, 1fr);
+  }
+`;
+
+const SportsSection = styled.section`
+  @media ${QUERIES.laptopAndUp} {
+    padding-left: 16px;
+  }
+`;
+
+const SportsStories = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 16px;
+
+  @media ${QUERIES.tabletAndUp} {  
+    display: flex;
+    gap: 16px;
+    max-width: 100%;
+    overflow: auto;
+
+    > * {
+      flex: 1 0 220px;
+    }
+  }
+`;
 
 export default SpecialtyStoryGrid;
